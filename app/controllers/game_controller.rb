383908bc77090cab4df4666fad560e97f4a4c207
tzequi-og <146213960+tzequi-og@users.noreply.github.com>
2023-10-25 13:53:01 +0000

    class3_rock

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,5 +1,15 @@
 class GameController < ApplicationController
   def user_played_rock
+    moves = ["rock","paper","scissors"]
+    @computer_move = moves.sample
+
+    if @computer_move == "rock"
+      @result = "tied"
+    elsif @computer_move == "paper"
+      @result = "lost"
+    elsif @computer_move == "scissors"
+      @result = "won"
+    end
     render({:template => "game_templates/play_rock"})
 
   end
