3eb47d42c9540c8fa7fb6807194b5a5005feb834
tzequi-og <146213960+tzequi-og@users.noreply.github.com>
2023-10-25 14:17:56 +0000

    final

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,17 +1,29 @@
 class GameController < ApplicationController
+  
+  def home
+    
+    render({:template => "game_templates/home"})
+  end
+
   def user_played_rock
     moves = ["rock","paper","scissors"]
     @computer_move = moves.sample
 
-    if @computer_move == "rock"
-      @result = "tied"
-    elsif @computer_move == "paper"
-      @result = "lost"
-    elsif @computer_move == "scissors"
-      @result = "won"
-    end
     render({:template => "game_templates/play_rock"})
+  end
 
+  def user_played_paper
+    moves = ["rock","paper","scissors"]
+    @computer_move = moves.sample
+
+    render({:template => "game_templates/play_paper"})
+  end
+
+  def user_played_scissors
+    moves = ["rock","paper","scissors"]
+    @computer_move = moves.sample
+
+    render({:template => "game_templates/play_scissors"})
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
 
+  get("/", { :controller =>"game", :action => "home" })
   get("/rock", { :controller =>"game", :action => "user_played_rock" })
+  get("/paper", { :controller =>"game", :action => "user_played_paper" })
+  get("/scissors", { :controller =>"game", :action => "user_played_scissors" })
 end
