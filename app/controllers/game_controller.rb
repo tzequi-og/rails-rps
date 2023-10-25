c9a1b7450e9b7dc6b104f7664a67e67c67594ddc
tzequi-og <146213960+tzequi-og@users.noreply.github.com>
2023-10-25 13:44:59 +0000

    class1

---
+++ app/controllers/game_controller.rb
@@ -0,0 +1,7 @@
+class GameController < ApplicationController
+  def user_played_rock
+    render({:plain => "hi!"})
+  end
+
+end
+  
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
 
+  get("/rock", { :controller =>"game", :action => "user_played_rock" })
 end
